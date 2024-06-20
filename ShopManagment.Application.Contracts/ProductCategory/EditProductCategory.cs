a97cd5f075dbc836d1a406c11e8f30dffb5d02cb
Mohammadreza Najafi <mr.najafi.tech@gmail.com>
2024-06-20 14:07:46 +0330

    Implement Create,Edit,Serach Model, View Model for ProductCategory Contract. Implement IProductCategoryApplication .

---
+++ ShopManagment.Application.Contracts/ProductCategory/EditProductCategory.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ShopManagment.Application.Contracts.ProductCategory
+{
+    public class EditProductCategory : CreateProductCategory
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ ShopManagment.Application.Contracts/ProductCategory/ProductCategorySearchModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ShopManagment.Application.Contracts.ProductCategory
+{
+    public class ProductCategorySearchModel
+    {
+        public string Name { get; set; }
+    }
+}
