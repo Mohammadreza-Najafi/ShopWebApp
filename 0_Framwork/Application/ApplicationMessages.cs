89b3802987cd8dee26dbd1e22a4254d78030c775
Mohammadreza Najafi <mr.najafi.tech@gmail.com>
2024-06-21 12:14:17 +0330

    Create some usage class for  App. Messages and generate slug

---
+++ 0_Framwork/Application/ApplicationMessages.cs
@@ -0,0 +1,10 @@
+﻿
+namespace _0_Framwork.Application
+{
+    public class ApplicationMessages
+    {
+        public const string DuplicatedRecord = "duplicated record, try again";
+        public const string RecordNotFound = "no record , try again";
+
+    }
+}
