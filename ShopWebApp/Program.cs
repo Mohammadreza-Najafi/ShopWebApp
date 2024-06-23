cccde2969f0ddab1183f4fb41adc7b1acda87beb
Mohammadreza Najafi <mr.najafi.tech@gmail.com>
2024-06-23 21:58:43 +0330

    Impelement project bootstrap and set services .

--- ShopWebApp/Program.cs
+++ ShopWebApp/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using ShopManagement.Configuration;
 using ShopManagement.Infrastructure.EFCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("ShopDB");
+
 builder.Services.AddDbContext<ShopContext>(options => options.UseSqlServer(connectionString));
+
+ShopManagementBootstrapper.Configure(builder.Services, connectionString);
 
 builder.Services.AddRazorPages();
 var app = builder.Build();
