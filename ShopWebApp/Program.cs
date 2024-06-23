b5de31e797303aa17de9382e030588fd58208a44
Mohammadreza Najafi <mr.najafi.tech@gmail.com>
2024-06-23 19:26:46 +0330

    Add first UI(HTML) .config appsetting.add services in program.cs and some config.

--- ShopWebApp/Program.cs
+++ ShopWebApp/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Infrastructure.EFCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("ShopDB");
+builder.Services.AddDbContext<ShopContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
 
 app.Run();
