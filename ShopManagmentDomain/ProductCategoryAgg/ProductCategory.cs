dfd8b1b8a0b31f96a773c3ca13cde8d6a19c50d7
Mohammadreza Najafi <mr.najafi.tech@gmail.com>
2024-06-26 23:18:54 +0330

    Add relation between product and category

--- ShopManagmentDomain/ProductCategoryAgg/ProductCategory.cs
+++ ShopManagmentDomain/ProductCategoryAgg/ProductCategory.cs
@@ -1,4 +1,5 @@
 ﻿using _0_Framwork.Domain;
+using ShopManagement.Domain.ProductAgg;
 
 namespace ShopManagement.Domain.ProductCategoryAgg
 {
@@ -13,7 +14,7 @@ namespace ShopManagement.Domain.ProductCategoryAgg
         public string Keywords { get; private set; }
         public string MetaDescription { get; private set; }
         public string Slug { get; private set; }
-
+        public List<Product> Products { get; private set; }
 
         public ProductCategory(string name, string description, string picture,
             string pictureAlt, string pictureTitle, string keywords, string metaDescription, string slug)
