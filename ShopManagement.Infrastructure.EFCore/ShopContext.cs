75a1e2c0653e4d38eeaa934a58c667751dfbd57c
Mohammadreza Najafi <mr.najafi.tech@gmail.com>
2024-06-30 22:56:57 +0330

    Implement Slide Repository and Slide Table struct and Add to Db Context.

--- ShopManagement.Infrastructure.EFCore/ShopContext.cs
+++ ShopManagement.Infrastructure.EFCore/ShopContext.cs
@@ -2,6 +2,7 @@
 using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Domain.ProductCategoryAgg;
 using ShopManagement.Domain.ProductPictureAgg;
+using ShopManagement.Domain.SlideAgg;
 using ShopManagement.Infrastructure.EFCore.Mapping;
 
 namespace ShopManagement.Infrastructure.EFCore
@@ -11,18 +12,17 @@ namespace ShopManagement.Infrastructure.EFCore
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductPicture> ProductPictures { get; set; }
+        public DbSet<Slide> Slides { get; set; }
 
         public ShopContext(DbContextOptions<ShopContext> options) : base(options)
         {
-
 
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
-            modelBuilder
-                .ApplyConfigurationsFromAssembly(typeof(ProductCategoryMapping).Assembly)
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductCategoryMapping).Assembly)
                 .ApplyConfigurationsFromAssembly(typeof(Product).Assembly)
                 .ApplyConfigurationsFromAssembly(typeof(ProductPicture).Assembly);
      
