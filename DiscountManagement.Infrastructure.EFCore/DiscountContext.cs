f03641fe3a2d5e8cb41867b0cb7d61295a57d310
Mohammadreza Najafi <mr.najafi.tech@gmail.com>
2024-07-10 20:50:36 +0330

    Implement Colleague Discount  Domain , Application ,Infrastructure and DB struct.

--- DiscountManagement.Infrastructure.EFCore/DiscountContext.cs
+++ DiscountManagement.Infrastructure.EFCore/DiscountContext.cs
@@ -1,4 +1,5 @@
-﻿using DiscountManagement.Domain.CustomerDiscountAgg;
+﻿using DiscountManagement.Domain.ColleagueDiscountAgg;
+using DiscountManagement.Domain.CustomerDiscountAgg;
 using DiscountManagement.Infrastructure.EFCore.Mapping;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +8,7 @@ namespace DiscountManagement.Infrastructure.EFCore
     public class DiscountContext : DbContext
     {
         public DbSet<CustomerDiscount> CustomerDiscounts { get; set; }
+        public DbSet<ColleagueDiscount> ColleagueDiscounts { get; set; }
 
         public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
         {
@@ -15,8 +17,9 @@ namespace DiscountManagement.Infrastructure.EFCore
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomerDiscountMapping).Assembly);                
+            modelBuilder
+                .ApplyConfigurationsFromAssembly(typeof(CustomerDiscountMapping).Assembly)
+                .ApplyConfigurationsFromAssembly(typeof(ColleagueDiscountMapping).Assembly);                
 
             base.OnModelCreating(modelBuilder);
         }
