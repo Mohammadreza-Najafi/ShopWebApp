06e1248cbeb150abbbd88bd3037ec527e16e1658
Mohammadreza Najafi <mr.najafi.tech@gmail.com>
2024-07-13 22:25:30 +0330

    resolved conflicts.

--- ShopWebApp/Program.cs
+++ ShopWebApp/Program.cs
@@ -1,6 +1,26 @@
+using DiscountManagement.Configuration;
+using InventoryManagement.Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Configuration;
+using ShopManagement.Infrastructure.EFCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("ShopDB");
+
+builder.Services.AddDbContext<ShopContext>(options => options.UseSqlServer(connectionString));
+
+ShopManagementBootstrapper.Configure(builder.Services, connectionString);
+DiscountManagementBootstrapper.Configure(builder.Services, connectionString);
+InventoryManagementBootstrapper.Configure(builder.Services, connectionString);
+
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
 
 app.Run();
